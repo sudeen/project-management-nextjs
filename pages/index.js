4208712431b97c877c0bb64c0d61635e1cf5fade
sudeen <ranjitkarsudeen14@gmail.com>
2020-03-30 00:45:35 +1100

    switch component added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-// import theme from "../src/ui/theme";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import AddIcon from "@material-ui/icons/Add";
+import Switch from "@material-ui/core/Switch";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const useStyles = makeStyles(theme => ({}));
 
@@ -10,10 +15,81 @@ export default function ProjectManager() {
   const classes = useStyles();
   const theme = useTheme();
 
+  const [websiteChecked, setWebsiteChecked] = useState(false);
+  const [iOSChecked, setiOSChecked] = useState(false);
+  const [androidChecked, setAndroidChecked] = useState(false);
+  const [softwareChecked, setSoftwareChecked] = useState(false);
+
   return (
     <Grid container direction="column">
-      <Grid item>
+      <Grid item style={{ marginTop: "2em", marginLeft: "5em" }}>
         <Typography variant="h1">Projects</Typography>
+      </Grid>
+      {/* Search or create block */}
+      <Grid item>
+        <TextField
+          placeholder="Search project details or create a new entry."
+          style={{ width: "35em", marginLeft: "5em" }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <AddIcon color="primary" />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+      {/* Switch block */}
+      <Grid item style={{ marginLeft: "5em", marginTop: "2em" }}>
+        <FormGroup row>
+          <FormControlLabel
+            style={{ marginRight: "5em" }}
+            control={
+              <Switch
+                checked={websiteChecked}
+                color="primary"
+                onChange={() => setWebsiteChecked(!websiteChecked)}
+              />
+            }
+            label="Websites"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            style={{ marginRight: "5em" }}
+            control={
+              <Switch
+                checked={iOSChecked}
+                color="primary"
+                onChange={() => setiOSChecked(!iOSChecked)}
+              />
+            }
+            label="iOS Apps"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            style={{ marginRight: "5em" }}
+            control={
+              <Switch
+                checked={androidChecked}
+                color="primary"
+                onChange={() => setAndroidChecked(!androidChecked)}
+              />
+            }
+            label="Android Apps"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={softwareChecked}
+                color="primary"
+                onChange={() => setSoftwareChecked(!softwareChecked)}
+              />
+            }
+            label="Custom Software"
+            labelPlacement="start"
+          />
+        </FormGroup>
       </Grid>
     </Grid>
   );
